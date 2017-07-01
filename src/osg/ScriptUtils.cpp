c321594eded8aeadb8338a0e169b874618f5146b
cxw <cxwembedded@gmail.com>
2017-06-30 23:07:39 -0400

    First working version of osg::ScriptUtils
    Added serializer with viewLookAt method.

---
+++ src/osg/ScriptUtils.cpp
@@ -0,0 +1,24 @@
+/* -*-c++-*- OpenSceneGraph - Copyright (C) 1998-2006 Robert Osfield
+ * This file Copyright (C) 2017 Chris White
+ *
+ * This library is open source and may be redistributed and/or modified under
+ * the terms of the OpenSceneGraph Public License (OSGPL) version 0.0 or
+ * (at your option) any later version.  The full license is in LICENSE file
+ * included with this distribution, and on the openscenegraph.org website.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * OpenSceneGraph Public License for more details.
+*/
+
+// This file is here to give ScriptUtils a place to live in the osg library.
+#include <osg/ScriptUtils>
+
+namespace osg {
+ScriptUtils::ScriptUtils()
+{
+    // do nothing
+}
+
+}
