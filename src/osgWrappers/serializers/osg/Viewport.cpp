dd8cb69ccf0b75fa416bca777c8dde346e81f074
Chris White <chrisw@leehayes.com>
2018-07-02 10:52:58 -0400

    Added osg::Viewport get() scripting method
    This permits scripts to get the current viewport position and size.

--- src/osgWrappers/serializers/osg/Viewport.cpp
+++ src/osgWrappers/serializers/osg/Viewport.cpp
@@ -1,14 +1,18 @@
 #include <osg/Viewport>
+#include <osg/Vec4f>
+#include <osg/ValueObject>
 #include <osgDB/ObjectWrapper>
 #include <osgDB/InputStream>
 #include <osgDB/OutputStream>
 
-static bool checkArea( const osg::Viewport& attr )
+namespace {
+
+bool checkArea( const osg::Viewport& attr )
 {
     return true;
 }
 
-static bool readArea( osgDB::InputStream& is, osg::Viewport& attr )
+bool readArea( osgDB::InputStream& is, osg::Viewport& attr )
 {
     double x, y, w, h;
     is >> x >> y >> w >> h;
@@ -16,11 +20,33 @@ static bool readArea( osgDB::InputStream& is, osg::Viewport& attr )
     return true;
 }
 
-static bool writeArea( osgDB::OutputStream& os, const osg::Viewport& attr )
+bool writeArea( osgDB::OutputStream& os, const osg::Viewport& attr )
 {
     os << attr.x() << attr.y() << attr.width() << attr.height() << std::endl;
     return true;
 }
+
+/// Vec4 get(): Return the viewport, packed as (x, y, width, height)
+struct GetViewport: public osgDB::MethodObject {
+    virtual bool run(osg::Object *objectPtr, osg::Parameters& ins,
+                osg::Parameters& outs) const
+    {
+        osg::Viewport *vp = dynamic_cast<osg::Viewport*>(objectPtr);
+        if(!vp) return false;
+
+        osg::Vec4 retval(
+            vp->x(),
+            vp->y(),
+            vp->width(),
+            vp->height()
+        );
+
+        outs.push_back(new osg::Vec4fValueObject(retval));
+        return true;
+    }
+}; //GetViewport
+
+} // anon namespace
 
 REGISTER_OBJECT_WRAPPER( Viewport,
                          new osg::Viewport,
@@ -28,4 +54,6 @@ REGISTER_OBJECT_WRAPPER( Viewport,
                          "osg::Object osg::StateAttribute osg::Viewport" )
 {
     ADD_USER_SERIALIZER( Area );  // _x, _y, _width, _height
+
+    ADD_METHOD_OBJECT( "get", GetViewport );
 }
