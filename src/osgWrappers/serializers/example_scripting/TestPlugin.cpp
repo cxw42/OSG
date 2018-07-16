7adf2078de9108121ec98ae74582b2907bc8e16a
Chris White <chrisw@leehayes.com>
2018-07-16 14:17:44 -0400

    First working version of virtual properties
    Note: the change in include/osgDB/Registry, in the constructor of
    PluginFunctionProxy, seemed to be required to get the function to run
    at DLL load on Cygwin.
    
    Thanks to https://www.nsnam.org/docs/linker-problems.doc for useful
    information about DLL loading in Cygwin.

--- src/osgWrappers/serializers/example_scripting/TestPlugin.cpp
+++ src/osgWrappers/serializers/example_scripting/TestPlugin.cpp
@@ -27,12 +27,41 @@
 #include <osgDB/Registry>
 #include <osg/Notify>
 
-namespace {
-extern "C" void reg() {
-    osg::ref_ptr<osgDB::ObjectWrapperManager> owm(osgDB::Registry::instance()->getObjectWrapperManager());
-    OSG_NOTICE << "Loaded TestPlugin" << std::endl;
-}
+#include <osgText/Text>
 
-// Load on startup
-osgDB::PluginFunctionProxy p1(reg);
-}
+/// Text property, but as a UTF-8 string rather than as an RW_USER serializer
+class Text8Serializer:
+    public osgDB::VirtualPropertySerializer<osgText::Text>
+{
+public:
+    typedef osgDB::VirtualPropertySerializer<osgText::Text> ParentType;
+    Text8Serializer(const char* name):
+        ParentType( name,
+                    osgDB::BaseSerializer::GET_SET_PROPERTY,
+                    osgDB::BaseSerializer::RW_STRING)
+    {}
+
+    virtual bool setPropertyFrom( osgDB::InputStream& is, TargetType& object )
+    {
+        std::string utf8chars;      ///< the new text
+        is >> utf8chars;        // works since #is is guaranteed to be binary
+        object.setText(utf8chars, osgText::String::ENCODING_UTF8);
+        return true;
+    }
+
+    virtual bool getPropertyInto( osgDB::OutputStream& os, const TargetType& object )
+    {
+        std::string utf8chars;      ///< the text in #object
+        utf8chars = object.getText().createUTF8EncodedString();
+        os << utf8chars;        // works since #os is guaranteed to be binary
+        return true;
+    }
+
+};
+
+BEGIN_EXTEND_OBJECT_WRAPPER( TestPlugin_osgText_Text, osgText::Text)
+    ADD_VIRTUAL_PROPERTY_SERIALIZER( Text8, Text8Serializer );
+END_EXTEND_OBJECT_WRAPPER()
+
+
+// vi: set ts=4 sts=4 sw=4 et ai: //
