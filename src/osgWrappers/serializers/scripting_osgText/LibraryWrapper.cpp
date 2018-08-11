478fd82a365e378cc349f78db8801966f675e6a2
Chris White <chrisw@leehayes.com>
2018-08-10 22:14:40 -0400

    Add LibraryWrapper for scripting_osgText

---
+++ src/osgWrappers/serializers/scripting_osgText/LibraryWrapper.cpp
@@ -0,0 +1,8 @@
+// TODO make sure this works for static linking - see
+// http://forum.openscenegraph.org/viewtopic.php?t=14841
+#include <osgDB/Registry>
+
+USE_EXTEND_OBJECT_WRAPPER(scripting_osgText_Text)
+USE_EXTEND_OBJECT_WRAPPER(scripting_osgText_TextBase)
+
+DECLARE_SERIALIZER_WRAPPER_LIBRARY(scripting_osgText)
